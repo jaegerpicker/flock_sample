623d1248c8b7a3d0b847eea283c5e24447943d37
Shawn Campbell <scampbell@vetsfirstchoice.com>
2017-04-21 12:56:35 -0400

    fixed tests, after time because of interuption

--- calc.js
+++ calc.js
@@ -1,16 +1,32 @@
 import R from 'ramda';
 let calc = {
-  add: (...nums) => {
-    return R.reduce(R.add, nums)();
+  add: nums => {
+    return R.reduce(R.add, 0, nums);
   },
-  sub: (...nums) => {
-    return R.reduce(R.subtract, nums);
+  sub: nums => {
+    return R.reduce(
+      (x, y) => {
+        return Math.abs(x - y);
+      },
+      0,
+      nums
+    );
   },
-  mutil: (...nums) => {
+  mutil: nums => {
     return R.reduce(R.multiply, 1, nums);
   },
-  div: (...nums) => {
-    return R.reduce(R.divide, nums);
+  div: nums => {
+    return R.reduce(
+      (x, y) => {
+        if (x > 0 && y > 0) {
+          return Math.abs(x / y);
+        } else {
+          return y;
+        }
+      },
+      0,
+      nums
+    );
   }
 };
 
